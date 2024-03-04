f5a66817f007aaa2ebb1efb752502d7df0ab3bfa
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-03-04 07:59:27 -0600

    Initial Commit

---
+++ multiplication/multiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplicationApp.swift
+//  multiplication
+//
+//  Created by Student on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct multiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
